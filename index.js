e9f8c001b7168daa7cd5d48c98fd54616efcb750
sapblatt11 <sapblatt11@gmail.com>
2018-01-05 00:25:43 -0500

    Iniital

---
+++ index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+
+//Don't touch from here down unless you know partially what you're doing
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+
+client.login(config.BOT_TOKEN).catch(console.error);
+client.on('ready', () => {
+  console.log('Now Online, ready to filter!');
+});
+
+
+client.on('message', message => {
+  if(config.FILTER_LIST.some(word => message.content.includes(word))){
+    message.delete()
+  }})
